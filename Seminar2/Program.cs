a3f36cb5192056315d16f0ce8ecda143554be8e1
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-24 20:38:26 +0300

    1 task

---
+++ Seminar2/Program.cs
@@ -0,0 +1,16 @@
+﻿int randomDigit = new Random().Next(10, 100);
+Console.WriteLine("Случ число: "+randomDigit);
+
+int ostatok = randomDigit%10;
+Console.WriteLine("Последняя цифра: "+ostatok);
+
+int firstDigit = randomDigit / 10;
+Console.WriteLine("Первая цифра: "+firstDigit);
+
+int max = firstDigit;
+
+if(ostatok > firstDigit)
+    max = ostatok;
+
+Console.WriteLine("Максимум: " +max);
+
