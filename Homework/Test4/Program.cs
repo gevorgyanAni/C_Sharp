6db55dd248dd2e0a097cf1fa1c26de8da7601ba2
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-27 22:30:25 +0300

    Четные от 1 до N

---
+++ Homework/Test4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 2;
+while (count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+count++;
+}
