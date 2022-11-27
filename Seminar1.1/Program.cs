29812af481938c165fa5dbc891f30350aa790db9
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-27 18:59:57 +0300

    Square

---
+++ Seminar1.1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {square}");
