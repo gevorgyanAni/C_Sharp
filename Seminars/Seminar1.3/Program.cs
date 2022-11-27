c860f3d85b07e774278b446ab7e410cad70c83d9
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-27 20:40:39 +0300

    Промежуток

---
+++ Seminars/Seminar1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count;
+if (number>0)
+    {
+        count = number*(-1);
+        while (count <= number)
+        {
+            Console.Write($"{count}, ");
+            count++;
+        }
+    }
+else
+{
+    count = number;
+    while (count <= number*(-1))
+        {
+            Console.Write($"{count}, ");
+            count++;
+        }
+}
