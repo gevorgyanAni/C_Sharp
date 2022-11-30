f631d81817c9446100c29854428edd4de4c42b69
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-30 22:20:10 +0300

    Нахождение третьей цифры

---
+++ Homework2/Test2/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = 0;
+if (number < 1000)
+{
+        result = number % 10;
+        if (number < 100)
+        {
+            Console.WriteLine("третьей цифры нет");
+        }
+        else 
+        {
+        Console.WriteLine(result);
+        }
+}
+else
+{
+while (number > 999)
+{   
+    number = number / 10;
+    if (number < 1000)
+    {
+    result = number % 10;
+    Console.WriteLine(result);
+    }
+}
+}
