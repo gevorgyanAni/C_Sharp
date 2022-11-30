020ad27884c6e49296f589d4fd74f197f475a052
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-11-30 22:24:10 +0300

    Нахождение второго числа

--- Homework2/Test1/Program.cs
+++ Homework2/Test1/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// 456 -> 5 456 /10 % 10=5
+// 456 -> 5 456 / 10 % 10=5
 // 782 -> 8
 // 918 -> 1
 
 
 Console.Clear();
 Console.Write("Введите число: ");
-int a = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 int result;
 
-result = a / 10 % 10;
-Console.WriteLine(result);
+result = number / 10 % 10;
+Console.WriteLine($"{number} -> {result}");
