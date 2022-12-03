b91894aa6afeaf5dec1340b2a8998ee60d5817b2
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-12-03 20:13:03 +0300

    Example2.2

---
+++ Seminar3.3/Test2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек
+//в этой четверти (x и y).
+Console.Clear();
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+
+}
+
+
+void FindQuarter(int ch)
+{
+    if (ch == 1) Console.WriteLine("(x > 0)  (y > 0)");
+    if (ch == 2) Console.WriteLine("(x < 0)  (y > 0)");
+    if (ch == 3) Console.WriteLine("(x < 0)  (y < 0)");
+    if (ch == 4) Console.WriteLine("(x > 0)  (y < 0)");
+}
+
+
+int ch = DataEntry("введите номер четверти = ");
+FindQuarter(ch);
