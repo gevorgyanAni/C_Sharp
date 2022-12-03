0030cc485a774086a904930e9c9706c04dda3dde
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-12-03 19:47:20 +0300

    Example1

---
+++ Seminar3.3/Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindQandrat(int x, int y)
+{
+    if ((x > 0) && (y > 0)) Console.WriteLine("1");
+    if ((x < 0) && (y > 0)) Console.WriteLine("2");
+    if ((x < 0) && (y < 0)) Console.WriteLine("3");
+    if ((x > 0) && (y < 0)) Console.WriteLine("4");
+}
+
+int x = DataEntryXY("Введите коориданты x = ");
+//Console.WriteLine(x);
+int y = DataEntryXY("Введите коориданты y = ");
+//Console.WriteLine(y);
+FindQandrat(x, y);
+
