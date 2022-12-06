448dec3bb795166442322fa23c94e7da7796e5f4
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-12-07 00:26:51 +0300

    Работа с методами

---
+++ Lecture2/Test1/Program.cs
@@ -0,0 +1,32 @@
+﻿// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while ( i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+    for( int i = 1; i <= 10; i++)
+    {
+       for( int j = 2; j <=10; j++) 
+       {
+            Console.WriteLine($"{i} x {j} = {i * j}");
+       }
+       Console.WriteLine();
+    }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
