979e6f0f229910280ba68e23c51599cc7941e6ff
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2022-12-11 23:12:58 +0300

    Test2

---
+++ Homework5/Test2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+const int SIZE = 8;
+const int LEFT_RANGE = -99;
+const int RIGHT_RANGE = 99;
+
+int[] array = new int[SIZE];
+Random ran = new Random();
+
+int[] FilleArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int SumOddNumber(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+array = FilleArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.Write('[' + string.Join(", ", array) + ']');
+int result = SumOddNumber(array);
+Console.WriteLine($" -> {result}");
