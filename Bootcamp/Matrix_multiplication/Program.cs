6bf0ee985b881a99b0dd053d264e97ec2e721d96
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2023-01-24 02:03:25 +0300

    Bootamp

---
+++ Bootcamp/Matrix_multiplication/Program.cs
@@ -0,0 +1,74 @@
+﻿int AddNumber(string str)
+{
+    System.Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+
+
+}
+
+void FillMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0, 6);
+        }
+    }
+
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+void MatrixMultiplication(int[,] first, int[,] second, int[,] multy)
+{
+    for (int i = 0; i < multy.GetLength(0); i++)
+    {
+        for (int j = 0; j < multy.GetLength(1); j++)
+        {
+            multy[i, j] = 0;
+            for (int k = 0; k < first.GetLength(1); k++)
+            {
+               multy[i, j] += first[i, k] * second[k, j];
+            }
+        }
+    }
+
+}
+
+
+int columnsFirstMatrix = AddNumber("Введите количесвто столбцов первой матрицы");
+int rowsFirstMatrix = AddNumber("Введите количесвто линий первой матрицы");
+int columnsSecondMatrix = AddNumber("Введите количесвто столбцов второй матрицы");
+int rowsSecondMatrix = AddNumber("Введите количесвто линий второй матрицы");
+
+if(columnsFirstMatrix == rowsSecondMatrix)
+{
+    int[,] firstMatrix = new int[columnsFirstMatrix, rowsFirstMatrix];
+    int[,] secondMatrix = new int[columnsSecondMatrix, rowsSecondMatrix];
+    FillMatrix(firstMatrix);
+    FillMatrix(secondMatrix);
+    PrintArray(firstMatrix);
+    System.Console.WriteLine();
+    PrintArray(secondMatrix);
+    int[,] multyMatrix = new int [columnsFirstMatrix, rowsSecondMatrix];
+    MatrixMultiplication(firstMatrix, secondMatrix, multyMatrix);
+    System.Console.WriteLine();
+    PrintArray(multyMatrix);
+}
+else
+    System.Console.WriteLine("Матрицы нельзя перемножить");
---
+++ Bootcamp/SpiralMassiv/Program.cs
@@ -0,0 +1,88 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int AddNumber(string str)
+{
+    System.Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// void FillMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0, 17);
+//         }
+//     }
+
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+
+}
+
+void SpiralMatrix(int[,] array, int rowsMatrix, int columnsMatrix)
+{
+    int count = 1;
+    int startCol = 0;
+    int endCol = columnsMatrix - 1;
+    int startRow = 0;
+    int endRow = rowsMatrix - 1;
+    while (startCol <= endCol && startRow <= endRow)
+    {
+        for (int i = startCol; i <= endCol; i++)
+        {
+            array[startRow, i] = count;
+            count++;
+        }
+        startRow++;
+
+        for (int j = startRow; j <= endRow; j++)
+        {
+            array[j, endCol] = count;
+            count++;
+        }
+        endCol--;
+
+        for (int i = endCol; i >= startCol; i--)
+        {
+            array[endRow, i] = count;
+            count++;
+        }
+        endRow--;
+
+        for (int j = endRow; j >= startRow; j--)
+        {
+            array[j, startCol] = count;
+            count++;
+        }
+        startCol++;
+
+
+    }
+
+}
+
+
+
+int rowsMatrix = AddNumber("Введите количесвто строк матрицы");
+int columnsMatrix = AddNumber("Введите количесвто столбцов матрицы");
+int[,] matrix = new int[rowsMatrix, columnsMatrix];
+SpiralMatrix(matrix, rowsMatrix, columnsMatrix);
+PrintMatrix(matrix);
