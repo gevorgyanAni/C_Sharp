a8a129e0ac593cfb8307770b2181d96ba04e4bb6
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2023-01-24 02:18:16 +0300

    Bootamp

--- Bootcamp/SpiralMassiv/Program.cs
+++ Bootcamp/SpiralMassiv/Program.cs
@@ -12,16 +12,6 @@ int AddNumber(string str)
     return number;
 }
 
-// void FillMatrix(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j] = new Random().Next(0, 17);
-//         }
-//     }
-
 
 void PrintMatrix(int[,] array)
 {
